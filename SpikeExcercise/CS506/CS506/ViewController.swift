6a1f43ac4ead8142ceaa8670f93de253861115fc
nikolapeevski <Jeratull@hotmail.com>
2019-02-18 15:42:48 -0600

    (type:chore) Add SpikeExcercise skeleton

---
+++ SpikeExcercise/CS506/CS506/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CS506
+//
+//  Created by Nikola Peevski on 13/02/2019.
+//  Copyright © 2019 Nikola Peevski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
