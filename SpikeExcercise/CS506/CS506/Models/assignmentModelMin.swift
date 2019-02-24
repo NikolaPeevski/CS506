97efe1cb7bd8267fe0846bfcef154b8b920b7763
nikolapeevski <Jeratull@hotmail.com>
2019-02-24 11:48:18 -0600

    (type:feature) Add Guest logic

---
+++ SpikeExcercise/CS506/CS506/Models/assignmentModelMin.swift
@@ -0,0 +1,9 @@
+//
+//  assignmentModelMin.swift
+//  CS506
+//
+//  Created by Nikola Peevski on 23/02/2019.
+//  Copyright © 2019 Nikola Peevski. All rights reserved.
+//
+
+import Foundation
---
+++ SpikeExcercise/CS506/CS506/ViewControllers/ReviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReviewViewController.swift
+//  CS506
+//
+//  Created by Nikola Peevski on 24/02/2019.
+//  Copyright © 2019 Nikola Peevski. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
